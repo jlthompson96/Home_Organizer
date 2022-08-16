7a17ad7b2c0d70234b4c28cda78d2fa3b9d5a6db
Joey Thompson <joeythompson1014@gmail.com>
2022-08-16 09:50:46 -0400

    Updated to Thymleaf

---
+++ src/test/java/com/organizer/stufforganizer/StufforganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.organizer.stufforganizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StufforganizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
