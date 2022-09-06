6b036521e0808c23501d94d6ec0a253ba83dfd8e
Joey Thompson <joeythompson1014@gmail.com>
2022-09-06 10:58:29 -0400

    Project Reorg

--- src/main/java/com/organizer/stufforganizer/Controller/giftCardController.java
+++ src/main/java/com/organizer/stufforganizer/Controller/giftCardController.java
@@ -1,10 +1,10 @@
-package com.organizer.stufforganizer;
+package com.organizer.stufforganizer.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class dashboardController {
+public class giftCardController {
 
     @GetMapping("/")
     public String getLoginPage(){
--- src/main/java/com/organizer/stufforganizer/Controller/wineTrackerController.java
+++ src/main/java/com/organizer/stufforganizer/Controller/wineTrackerController.java
@@ -1,4 +1,4 @@
-package com.organizer.stufforganizer;
+package com.organizer.stufforganizer.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
