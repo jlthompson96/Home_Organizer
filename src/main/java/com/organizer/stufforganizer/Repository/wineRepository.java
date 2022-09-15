0bc7c614763cfb0b4cc14144de5c6400f0cde448
Joey Thompson <joeythompson1014@gmail.com>
2022-09-15 13:40:51 -0400

    V0.5 (#13)
    * Added Lombok to project.
    
    * Renamed dashboard to giftcardfuelpoints
    
    * JTOR-2

---
+++ src/main/java/com/organizer/stufforganizer/Repository/wineRepository.java
@@ -0,0 +1,8 @@
+package com.organizer.stufforganizer.Repository;
+
+import com.organizer.stufforganizer.Entity.wineEntity;
+import org.springframework.data.repository.CrudRepository;
+
+public interface wineRepository extends CrudRepository<wineEntity, Integer> {
+}
+
